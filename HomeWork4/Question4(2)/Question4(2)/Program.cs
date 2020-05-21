b4e2af0037c2f507bfd722ab5e1e3b19ca05cbd5
Liuwensong <1352399340@qq.com>
2020-05-21 20:43:00 +0800

    HomeWork

---
+++ HomeWork4/Question4(2)/Question4(2)/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Question4_2_
+{
+    public delegate void ClockEvent();
+    public class Clock
+    {
+        public event ClockEvent Tick;
+        public event ClockEvent Alarm;
+        public void Run(int x)
+        {
+            if (x % 5 == 0)
+                Alarm();
+            else
+                Tick();
+        }
+    }
+    public class Reality
+    {
+        public Clock clock1 = new Clock();
+        public Reality()
+        {
+            clock1.Tick += clock_Tick;
+            clock1.Alarm += clock_Alarm;
+        }
+        void clock_Tick()
+        {
+            Console.WriteLine("闹钟正在走时。");
+        }
+        void clock_Alarm()
+        {
+            Console.WriteLine("闹钟正在响铃。");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reality reality = new Reality();
+            for(int i=1;i<31;i++)
+            {
+                reality.clock1.Run(i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
